8d93ff01bdd7d9bd27c18015e7edbf3d0395400a
Chadmichaelg <chadmgrubbs@gmail.com>
2022-04-18 22:36:49 +0000

    initial commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,15 +1,69 @@
-import React from "react";
+import React, { useSatate, useEffect, useRef } from "react";
+import TodoList from "./TodoList";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+
+function Home() {
 	return (
-		<div>
-			
+		<div className="todo-app">
+			<TodoList />
 		</div>
 	);
-};
+}
+
+// const Home = () => {
+// 	const [Todo, setTodos] = useState([{ text: "Do homework", isEdit: false }]);
+// 	// useEffect(() => {}, [inputRefs]);
+
+// 	const inputRefs = useRef(null)
+
+// 	const [textValue, setTextValue] = useState("");
+
+// 	const add = () => {
+// 		console.log("click");
+// 		let newArray = [...Todo];
+// 		newArray.push({ text: ref.current.value, isEdit: false });
+// 		console.log(ref.current.value);
+// 		setTodos(newArray);
+// 	};
+
+// 	const handleInput = (event) => {
+// 		setTextValue(event.target.value);
+// 		console.log(event.target.value);
+// 	};
+
+// 	useEffect(() => {}, [inputRefs]);
+
+// 	return (
+// 		<div>
+// 			<div>
+// 				<input
+// 					ref={inputRefs}
+// 							//  onChange={() => {setTextValue(event.target.value)}}
+// 							>
+// 							</input>
+// 							<button type="button" onclick={add}>
+// 								Add
+// 							</button>
+// 						</div>
+// 						{Todo.map((Todo, index) => {
+// 						// {Todo.text}
+// 						<div>
+// 							<button type="button" onclick={""}>
+// 								Edit
+// 							</button>
+// 							<button type="button" onclick={""}>
+// 								Delete
+// 							</button>
+// 						</div>
+
+// 				);
+// 			})}
+// 		</div>
+// 	);
+// };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,9 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+// import Todo from "./component/Todo.js";
+// import TodoForm from "./component/TodoForm.js";
+// import TodoList from "./component/TodoList.js";
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
